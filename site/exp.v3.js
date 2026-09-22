521b197eb7dfd4ac37acb099a8f65441bd55d664
kingnazz <117854838+kingnazz@users.noreply.github.com>
2026-09-21 18:12:27 -0700

    Update site/exp.v3.js for 1.2.1

--- site/exp.v3.js
+++ site/exp.v3.js
@@ -19,7 +19,7 @@
 
   var REPO = "nazar-exp/EXP-IP-Scanner";
   var RELEASES = "https://github.com/" + REPO + "/releases";
-  var SCREENSHOT_VERSION = "1.2.0";
+  var SCREENSHOT_VERSION = "1.2.1";
 
   // ----------------------------------------------------- screenshot switcher
 
