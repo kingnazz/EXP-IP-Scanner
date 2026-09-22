52f3a8043bcfac154387804dc9ceb81dc0c10b6e
kingnazz <117854838+kingnazz@users.noreply.github.com>
2026-09-21 20:53:39 -0700

    Remove Backstage download asset rules

--- site/exp.v3.js
+++ site/exp.v3.js
@@ -75,13 +75,9 @@
       must: [/^EXP-IP-Scanner_[\d.]+_windows-x64-portable\.zip$/i],
       mustNot: [/arm64|aarch64/i, /setup/i],
     },
-    backstage: {
-      must: [/^EXP-IP-Scanner_[\d.]+_backstage-x64\.exe$/i],
-      mustNot: [/arm64|aarch64/i, /portable/i, /setup/i],
-    },
     installer: {
       must: [/^EXP-IP-Scanner_[\d.]+_x64-setup\.exe$/i],
-      mustNot: [/arm64|aarch64/i, /portable/i, /backstage/i],
+      mustNot: [/arm64|aarch64/i, /portable/i],
     },
   };
 
